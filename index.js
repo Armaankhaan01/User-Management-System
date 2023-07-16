82b4d4f7406f2ec8a26a0f7ec71e13b63d18abcd
Arman Khan <Armankhan876426@gmail.com>
2023-07-16 17:18:06 +0530

    successful

--- index.js
+++ index.js
@@ -88,22 +88,22 @@ app.get("/chart", async (req, res) => {
   }
 });
 
-app.get("/networkSpeed", async (req, res) => {
-  try {
-    const speedTest = new SpeedTest({
-      token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
-    });
-    const speed = await speedTest.getSpeed();
+// app.get("/networkSpeed", async (req, res) => {
+//   try {
+//     const speedTest = new SpeedTest({
+//       token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
+//     });
+//     const speed = await speedTest.getSpeed();
 
-    // Convert speed to Mbps
-    const speedMbps = ((speed / 1024 / 1024) * 8).toFixed(2);
+//     // Convert speed to Mbps
+//     const speedMbps = ((speed / 1024 / 1024) * 8).toFixed(2);
 
-    res.json({ speed: speedMbps });
-  } catch (error) {
-    console.log("Error fetching network speed:", error);
-    res.render("error");
-  }
-});
+//     res.json({ speed: speedMbps });
+//   } catch (error) {
+//     console.log("Error fetching network speed:", error);
+//     res.render("error");
+//   }
+// });
 
 // Handle 404
 app.get("*", (req, res) => {
